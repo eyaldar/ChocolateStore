eb54754345577b6a1fc364030733f32dda381b4e
eyaldar <eyaldar@gmail.com>
2015-01-05 02:57:01 +0200

    Now tries recursively tries to get dependencies of the requested package

--- src/ChocolateStore/PackageCacher.cs
+++ src/ChocolateStore/PackageCacher.cs
@@ -5,93 +5,147 @@ using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using Ionic.Zip;
+using System.Xml.Linq;
+using System.Dynamic;
+using System.Collections.Generic;
 
 namespace ChocolateStore
 {
-	class PackageCacher
-	{
+    class PackageCacher
+    {
 
-		private const string INSTALL_FILE = "tools/chocolateyInstall.ps1";
+        private const string INSTALL_FILE = "tools/chocolateyInstall.ps1";
 
-		public delegate void FileHandler(string fileName);
-		public delegate void DownloadFailedHandler(string url, Exception ex);
+        public delegate void FileHandler(string fileName);
+        public delegate void DownloadFailedHandler(string url, Exception ex);
 
-		public event FileHandler SkippingFile = delegate { };
-		public event FileHandler DownloadingFile = delegate { };		
-		public event DownloadFailedHandler DownloadFailed = delegate { };
+        public event FileHandler SkippingFile = delegate { };
+        public event FileHandler DownloadingFile = delegate { };
+        public event DownloadFailedHandler DownloadFailed = delegate { };
 
-		public void CachePackage(string dir, string url)
-		{
-			var packagePath = DownloadFile(url, dir);
+        public void CachePackage(string dir, string url)
+        {
+            var packagePath = DownloadFile(url, dir);
 
-			using (var zip = ZipFile.Read(packagePath))
-			{
-				var entry = zip.FirstOrDefault(x => string.Equals(x.FileName, INSTALL_FILE, StringComparison.OrdinalIgnoreCase));
+            using (var zip = ZipFile.Read(packagePath))
+            {
+                var packageName = Path.GetFileNameWithoutExtension(packagePath);
 
-				if (entry != null) {
-					string content = null;
-					var packageName = Path.GetFileNameWithoutExtension(packagePath);
+                var entry = zip.FirstOrDefault(x => string.Equals(x.FileName, INSTALL_FILE, StringComparison.OrdinalIgnoreCase));
 
-					using (MemoryStream ms = new MemoryStream()) {
-						entry.Extract(ms);
-						content = Encoding.UTF7.GetString(ms.ToArray());
-					}
+                if (entry != null)
+                {
+                    string content = GetZipEntryContent(entry);
 
-					content = CacheUrlFiles(Path.Combine(dir, packageName), content);
-					zip.UpdateEntry(INSTALL_FILE, content);
-					zip.Save();
+                    string newContent = CacheUrlFiles(Path.Combine(dir, packageName), content);
+                    newContent.Remove(0, newContent.IndexOf(content.First()));
+                    zip.UpdateEntry(INSTALL_FILE, newContent);
+                    zip.Save();
 
-				}
+                }
 
-			}
+                GetDependencies(dir, url, zip);
+            }
+        }
 
-		}
+        private void GetDependencies(string dir, string url, ZipFile zip)
+        {
+            var nuspecFile = zip.FirstOrDefault(x => x.FileName.EndsWith("nuspec", StringComparison.OrdinalIgnoreCase));
+            List<string> dependenciesList = GetDependenciesListFromNuspec(nuspecFile);
 
-		private string CacheUrlFiles(string folder, string content)
-		{
+            var packageNameWithoutVersion = Path.GetFileNameWithoutExtension(nuspecFile.FileName);
+            var urlFormat = url.Remove(url.IndexOf(packageNameWithoutVersion)) + "{0}";
 
-			const string pattern = "(?<=['\"])http\\S*(?=['\"])";
+            GetDependencies(dependenciesList, dir, urlFormat);
+        }
 
-			if (!Directory.Exists(folder)) {
-				Directory.CreateDirectory(folder);
-			}
+        private void GetDependencies(List<string> dependenciesList, string dir, string url)
+        {
+            foreach (var dependecy in dependenciesList)
+            {
+                CachePackage(dir, string.Format(url, dependecy));
+            }
+        }
 
-			return Regex.Replace(content, pattern, new MatchEvaluator(m => DownloadFile(m.Value, folder)));
+        private static string GetZipEntryContent(ZipEntry entry)
+        {
+            string content = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                entry.Extract(ms);
+                content = Encoding.Default.GetString(ms.ToArray());
+            }
+            return content;
+        }
 
-		}
+        private static List<string> GetDependenciesListFromNuspec(ZipEntry nuspecFileEntry)
+        {
+            ExpandoObject root = new ExpandoObject();
+            List<string> dependenciesList = new List<string>();
+            string content = GetZipEntryContent(nuspecFileEntry);
+            var xDoc = XDocument.Load(new StringReader(content));
 
-		private string DownloadFile(string url, string destination)
-		{
+            XmlToDynamic.Parse(root, xDoc.Elements().First(), "dependencies");
 
-			try
-			{
-				var request = WebRequest.Create(url);
-				var response = request.GetResponse();
-				var fileName = Path.GetFileName(response.ResponseUri.LocalPath);
-				var filePath = Path.Combine(destination, fileName);
+            if (root.Any())
+            {
+                dynamic result = root;
+                var dependencies = result.dependencies as List<dynamic>;
+                foreach (var dependency in dependencies)
+                {
+                    var realDependency = dependency as IDictionary<String, object>;
+                    dependenciesList.Add(realDependency["id"] as string);
+                }
+            }
 
-				if (File.Exists(filePath))
-				{
-					SkippingFile(fileName);
-				}
-				else
-				{
-					DownloadingFile(fileName);
-					using (var fs = File.Create(filePath))
-					{
-						response.GetResponseStream().CopyTo(fs);
-					}
-				}
+            return dependenciesList;
+        }
 
-				return filePath;
-			}
-			catch (Exception ex)
-			{
-				DownloadFailed(url, ex);
-				return url;
-			}
+        private string CacheUrlFiles(string folder, string content)
+        {
 
-		}
+            const string pattern = "(?<=['\"])http\\S*(?=['\"])";
 
-	}
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            return Regex.Replace(content, pattern, new MatchEvaluator(m => DownloadFile(m.Value, folder)));
+
+        }
+
+        private string DownloadFile(string url, string destination)
+        {
+            try
+            {
+                var request = WebRequest.Create(url);
+                var response = request.GetResponse();
+                var fileName = Path.GetFileName(response.ResponseUri.LocalPath);
+                var filePath = Path.Combine(destination, fileName);
+
+                if (File.Exists(filePath))
+                {
+                    SkippingFile(fileName);
+                }
+                else
+                {
+                    DownloadingFile(fileName);
+                    using (var fs = File.Create(filePath))
+                    {
+                        response.GetResponseStream().CopyTo(fs);
+                    }
+                }
+
+                return filePath;
+            }
+            catch (Exception ex)
+            {
+                DownloadFailed(url, ex);
+                return url;
+            }
+
+        }
+
+    }
 }
---
+++ src/ChocolateStore/XmlToDynamic.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ChocolateStore
+{
+    public class XmlToDynamic
+    {
+        public static void Parse(dynamic parent, XElement node, string rootElementName)
+        {
+            var item = new ExpandoObject();
+
+            if (node.Name.LocalName == rootElementName)
+            {
+                Parse(parent, node);
+            }
+            else if (node.HasElements)
+            {
+                foreach (var element in node.Elements())
+                {
+                    Parse(parent, element, rootElementName);
+                }
+            }
+        }
+
+        private static void Parse(dynamic parent, XElement node)
+        {
+            var item = new ExpandoObject();
+
+            if (node.HasElements)
+            {
+                var list = new List<dynamic>();
+                foreach (var element in node.Elements())
+                {
+                    Parse(list, element);
+                }
+
+                //AddProperty(item, node.Elements().First().Name.LocalName, list);
+                AddProperty(parent, node.Name.LocalName, list);
+            }
+            else
+            {
+                AddAttributes(node, item);
+                AddProperty(parent, node.Name.LocalName, item);
+            }
+        }
+
+        private static void AddAttributes(XElement node, ExpandoObject item)
+        {
+            foreach (var attribute in node.Attributes())
+            {
+                if (!attribute.Name.LocalName.Equals("xmlns"))
+                {
+                    AddProperty(item, attribute.Name.LocalName, attribute.Value.Trim());
+                }
+            }
+        }
+
+        private static void AddProperty(dynamic parent, string name, object value)
+        {
+            if (parent is List<dynamic>)
+            {
+                (parent as List<dynamic>).Add(value);
+            }
+            else
+            {
+                (parent as IDictionary<String, object>)[name] = value;
+            }
+        }
+    }
+}
